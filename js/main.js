9b89b5afa6b0162f0354723625dabb7bcad05f18
Pablo Molins <pablo.molins@gmail.com>
2017-02-15 18:54:42 +0100

    Starting to simplify page

--- js/main.js
+++ js/main.js
@@ -1,5 +1,19 @@
+function init() {
+	var minutes = ('00' + time / 60).slice(-2)
+	var seconds = ('00' + time % 60).slice(-2)
+
+	var text = minutes + ':' + seconds;
+
+	$('#chrono').text(text);
+
+	$('#initButton').prop("disabled", false);
+	$('#pauseButton').prop("disabled", true);
+	$('#stopButton').prop("disabled", true);
+}
+
+
 function initChrono() {
-	final = Date.now() + time;
+	final = performance.now() + time * 1000;
 
 	animationFrame = requestAnimationFrame(chron);
 
@@ -30,23 +44,24 @@ function resetChrono() {
 	var min = $('#minutesInput').value();
 	var sec = $('#secondsInput').value();
 
-	time = (min * 60 + sec) * 1000;
+	time = (min * 60 + sec);
 }
 
 function changeTime(t) {
-	time = t * 1000;
+	time = t;
 	stopChrono();
-	var final = Date.now() + time;
+	final = performance.now() + time;
 	chron(final);
 }
 
 // Private 
-var time = 420000;
+var time = 420;
 var final = null;
 var animationFrame = null;
 
 function chron(t) {
-	var value = (final - t) / 1000; 
+	var value = (final - t) / 1000;
+	console.log(t);
 
 	var minutes = ('00' + Math.floor(value / 60)).slice(-2)
 	var seconds = ('00' + Math.floor(value % 60)).slice(-2)
